ca44b40fe9a97d43bdaeeefeb49ca3d53d7d093d
Mahinuzzaman <nidaghmahin@gmail.com>
2019-08-29 17:18:06 +0600

    Add files via upload

---
+++ cf.278.2.a.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int cec(long long int a)
+{
+    while(a!=0)
+    {
+        if(a%10==8 || a%10==-8) return 1;
+        a=a/10;
+    }
+    return 0;
+}
+int main()
+{
+    long long int n;
+    while(cin>>n)
+    {
+ 
+            long long int h=n+1;
+            for( ; ; h++)
+            {
+                if(cec(h)==1)
+                    break;
+            }
+            cout<<h-n<<"\n";
+    }
+    return 0;
+}
