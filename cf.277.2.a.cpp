abe1125a18ecb72e55db801604ea91bbd1c3ffc7
Mahinuzzaman <nidaghmahin@gmail.com>
2019-08-29 17:16:09 +0600

    Add files via upload

---
+++ cf.277.2.a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    long long int n;
+    while(cin>>n)
+    {
+        if(n%2==0)
+        {
+            cout<<n/2<<endl;
+        }
+        else cout<<((n/2)+1)*(-1)<<endl;
+    }
+}
